b1f8b37b177ec852773abd9f20f6e444503d6fae
bootstraponline <matt@bootstraponline.com>
2018-03-26 13:06:24 -0400

    Add cancel build step

---
+++ step.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+curl -X POST \
+  https://api.bitrise.io/v0.1/apps/$BITRISE_APP_SLUG/builds/$BITRISE_BUILD_SLUG/abort \
+  -H "authorization: token $BITRISE_TOKEN" \
+  -H 'content-type: application/json; charset=UTF-8' \
+  -d '{
+  "abort_reason": "Build cancelled.",
+    "skip_notifications": true,
+    "abort_with_success": true
+}'
